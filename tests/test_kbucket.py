317b85313a824747014858a5c1b0ba441120f857
Willem Victor <wimpievictor@gmail.com>
2019-12-07 09:30:49 +0200

    Implemented kbucket sort

--- tests/test_kbucket.py
+++ tests/test_kbucket.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from dht.route import NodeId, KBucket, Contact
 
@@ -10,3 +11,23 @@ class TestKBucket(unittest.TestCase):
         bucket.add(Contact('127.0.0.1', 9090, NodeId(0x1000)))
 
         self.assertTrue(bucket.contains(NodeId(0x1000)))
+
+    def test_sort(self):
+        """
+        Should sort contacts by last seen datetime.
+        """
+        # assume
+        bucket = KBucket()
+        bucket.add(Contact(None, None, NodeId(7)))
+        bucket.add(Contact(None, None, NodeId(3)))
+        bucket.add(Contact(None, None, NodeId(1)))
+
+        bucket.get(NodeId(1)).last_seen = datetime(2019, 9, 2)
+        bucket.get(NodeId(7)).last_seen = datetime(2019, 9, 3)
+        bucket.get(NodeId(3)).last_seen = datetime(2019, 10, 1)
+
+        # act
+        bucket.sort()
+
+        # assert
+        self.assertEqual(NodeId(3), bucket._contacts[-1].node_id, )
