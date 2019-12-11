6cfcfd4b3a76ddd8c0bef88ca7baaa8f8dd5088b
Willem Victor <wimpievictor@gmail.com>
2019-12-11 21:26:38 +0200

    Unmarshalling response

--- dht/client.py
+++ dht/client.py
@@ -3,6 +3,7 @@ UDP client for connecting to a Peer, but not participating in the network.
 """
 from gevent import socket
 
+from dht.messages import MessageMeta
 from dht.utils import create_logger
 
 
@@ -16,6 +17,7 @@ class DhtClient(object):
         Send a message to the given peer address, and returns the response message.
 
         :param address: IP address of peer.
+        :param port: Peer port to connect to.
         :param message: Message payload to send.
         :return: Response message.
         """
@@ -28,3 +30,10 @@ class DhtClient(object):
             # Response
             data, _ = sock.recvfrom(8192)
             self._logger.info("Received {}".format(data))
+
+            # Assumes that message types are registered in message meta registry.
+            # TODO: throw exception when message type is unknown, or unmarshalling fails
+            enum_bytes = data[:4]
+            message_enum = int.from_bytes(enum_bytes, 'big')
+            message_type = MessageMeta.message_types().get(message_enum, None)
+            return message_type.unmarshal(data[4:])
--- dht/client_cli.py
+++ dht/client_cli.py
@@ -1,21 +1,52 @@
+import importlib
 
 import click
 
 from dht.client import DhtClient
 from dht.utils import create_logger
-from dht.requests import *
 
 logger = create_logger(__name__)
+
+
+def import_all(module):
+    """
+    Emulates importing all from module. `from module import *`
+
+    This is accomplished by importing the module, determining which
+    members need to be imported, and inserting them into the global
+    scope.
+    """
+    # get a handle on the module
+    mdl = importlib.import_module(module)
+
+    # is there an __all__?  if so respect it
+    if "__all__" in mdl.__dict__:
+        names = mdl.__dict__["__all__"]
+    else:
+        # otherwise we import all names that don't begin with _
+        names = [x for x in mdl.__dict__ if not x.startswith("_")]
+
+    # now drag them in
+    globals().update({k: getattr(mdl, k) for k in names})
 
 
 @click.command()
 @click.option('--address', default='127.0.0.1')
 @click.option('--port', default=9000)
+@click.option('--module', default='dht.requests',
+              help="Module that contains the request and response messages. Default: 'dht.requests'")
 @click.argument('message')
-def send(address, port, message):
+def send(address, port, module, message):
+    import_all(module)
+
     client = DhtClient()
 
-    client.send(address, port, PingRequest())
+    # TODO: Safely parse given `message` into request object
+    request = PingRequest()
+    logger.info("Sending Message %s" % request)
+
+    response = client.send(address, port, request)
+    logger.info("Received Message %s" % repr(response))
 
 
 if __name__ == '__main__':
